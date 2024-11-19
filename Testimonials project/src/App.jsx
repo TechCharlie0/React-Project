ce9f2106c320633281b20b98af31dafe46949b33
isha kandel <ishachttri@gmail.com>
2024-11-19 23:42:44 +0545

    done testimonials

--- Testimonials project/src/App.jsx
+++ Testimonials project/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reviews from './data';
+import './App.css';
+import Testimonials from './components/Testimonials';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='flex flex-col w-[100vw] h-[100vh] justify-center items-center bg-gray-200'>
+      <div className='text-center'>
+        <h1 className='text-4xl font-bold'>Our Testimonials</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="bg-violet-400 h-[4px] w-[1/5] mt-3 mx-auto"></div>
+
+      <Testimonials reviews={reviews} />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ Testimonials project/src/data.jsx
@@ -0,0 +1,39 @@
+const reviews = [
+    {
+        id: 1,
+        name: "Isha Kandel",
+        job: "Software Engineer", 
+        image: "https://aeccc.targettechnology.in/static/media/PranayGupta.f3c530b7630ba8efb2ab.jpg",
+        text: "I’m always excited to learn new technologies and stay up-to-date with the latest trends in the tech industry. I have had the pleasure of working with this team on several projects, and I am always excited .",
+    },
+    {
+        id: 2,
+        name: "Sahun Kandel",
+        job: "Computer Engineer", 
+        image: "https://aeccc.targettechnology.in/static/media/AbirPal.574a09ad7cb325853b29.jpg",
+        text: "I have had the pleasure of working with this team on several projects, and I am always excited Over the years, I’ve had the opportunity to work on a variety of projects, including [mention a specific.",
+    },
+    {
+        id: 3,
+        name: "Robert Pattinson",
+        job: "Software Engineer", 
+        image: "https://aeccc.targettechnology.in/static/media/SoumyaBanerjee.2e2521d6029842435080.jpg",
+        text: "you can find me [mention a personal hobby or interest, e.g.,exploring new coffee shops, reading tech blogs, or experimenting with UI designsI have had the pleasure of working Over the years, I’ve had the opportunity to work on a variety of projects, including mention a specificwith this team on several projects, and I am always excited.",
+    },
+    {
+        id: 4,
+        name: "Robisah kandel",
+        job: "Software Engineer", 
+        image: "https://aeccc.targettechnology.in/static/media/SaikatMukherjee.033310703edff52d0532.jpg",
+        text: "When I'm not coding, you can find me [a personal touch, e.g.exploring new coffee spots, learning about design trends, or watching tutorials to sharpen my skills. I'm always looking to grow and collaborate with like-minded individuals who are eager to build meaningful and impactful projects.",
+    },
+    {
+        id: 5, 
+        name: "IshaRobert pattinson",
+        job: "Software Engineer", 
+        image: "https://aeccc.targettechnology.in/static/media/PranayGupta.f3c530b7630ba8efb2ab.jpg",
+        text: "I thrive on learning new technologies, solving problems, and turning ideas into reality.I have had the pleasure of working with this team on several projects, and I am always excited.",
+    },
+];
+export default reviews;
+
